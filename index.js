d8c1aa6caa099fe7d726647082a8665761942aa6
Maira Berdun <mberdun.95@gmail.com>
2021-09-06 18:56:43 -0300

    fixed roots

--- index.js
+++ index.js
@@ -11,37 +11,37 @@ for (let i = 0; i < numberOfButtons; i++) {
 
         switch (especie) { //si el inner del boton toma esos valores...
             case "ave":
-                let ave = new Audio('sounds/pajaro.mp3'); //creamos una nueva variable que guarda un nuevo ojeto donde especificamos el archivo
+                let ave = new Audio('./sounds/pajaro.mp3'); //creamos una nueva variable que guarda un nuevo ojeto donde especificamos el archivo
                 ave.play(); // lo que se ejecutara - play es un metodo
                 break;
 
             case "elefante":
-                let elefante  = new Audio('sounds/elefante.mp3'); 
+                let elefante  = new Audio('./sounds/elefante.mp3'); 
                 elefante.play();
                 break;
         
             case "perro":
-                let perro  = new Audio('sounds/perro.mp3'); 
+                let perro  = new Audio('./sounds/perro.mp3'); 
                 perro.play();
                 break;
 
             case "tigre":
-                let tigre  = new Audio('sounds/tigre.mp3'); 
+                let tigre  = new Audio('./sounds/tigre.mp3'); 
                 tigre.play();
                 break;
         
             case "gato":
-                let gato  = new Audio('sounds/gato.mp3'); 
+                let gato  = new Audio('./sounds/gato.mp3'); 
                 gato.play();
                 break;
 
             case "vaca":
-                let vaca  = new Audio('sounds/vaca.mp3'); 
+                let vaca  = new Audio('./sounds/vaca.mp3'); 
                 vaca.play();
                 break;
             
             case "zorro":
-                let zorro  = new Audio('sounds/zorro.mp3'); 
+                let zorro  = new Audio('./sounds/zorro.mp3'); 
                 zorro.play();
                 break;
 
